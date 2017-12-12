8b18f1a2d12d85ea6a1133b6a8ad6088e10a47d4
Fabien Lebas <fabien.lebas@decathlon.com>
2017-12-12 22:46:02 +0100

    Translation to English, let's get international first

--- decision.js
+++ decision.js
@@ -25,89 +25,85 @@ request(
     method: "GET"
   },
   function(error, response, resultAPI) {
-    console.log("error:", error); // Print the error if one occurred
-    console.log("statusCode:", response && response.statusCode); // Print the response status code if a response was received
-    const returnedData = JSON.parse(resultAPI);
-    const today = new Date();
-    const year = today.getFullYear();
-    const month = today.getMonth() + 1;
-    const day = today.getDate();
-    const dayOfTheWeek = today.getDay();
-    const currentHour = today.getHours();
-    const now = `${year}-${month}-${day} ${currentHour}`;
+    if (error){
+      console.log("error:", error); // Print the error if one occurred
+    } else {
+      const returnedData = JSON.parse(resultAPI);
 
-    let returnedTimes = returnedData.list.map((object) => {
-        return {
-          time: object.dt_txt,
-          date: object.dt_txt.split(" ")[0],
-          day: (new Date(object.dt_txt.split(" ")[0])).getDay(),
-          hour: object.dt_txt.split(" ")[1].split(":")[0],
-          rain: Math.round(valueOr0(object.rain)*100)/100,
-          snow: Math.round(valueOr0(object.snow)*100)/100,
-          temp: Math.round(object.main.temp - 273.15)*10/10,
-          wind: Math.round(object.wind.speed*100)/100,
-          bikeExplanation: "",
-          bikeDecision: false,
-          bike : function(dayParam){
-            if (userLimits.daysOff.find((number) => number === dayParam) !== undefined){
-              this.bikeExplanation = "week-end";
-            } else if (userLimits.rain < this.rain){
-              this.bikeExplanation = `pluie ${this.rain}mm`;
-            } else if (userLimits.snow < this.snow){
-              this.bikeExplanation = `neige ${this.snow}mm`;
-            } else if (userLimits.wind < this.wind){
-              this.bikeExplanation = `trop de vent ${this.wind}km/h`;
-            } else if (userLimits.temp < this.temp - 273.15){
-              this.bikeExplanation = `trop froid ${this.temp}°C`
-            } else{
-              this.bikeDecision = true;
+      let returnedTimes = returnedData.list.map((object) => {
+          return {
+            time: object.dt_txt,
+            date: object.dt_txt.split(" ")[0],
+            day: (new Date(object.dt_txt.split(" ")[0])).getDay(),
+            hour: object.dt_txt.split(" ")[1].split(":")[0],
+            rain: Math.round(valueOr0(object.rain)*100)/100,
+            snow: Math.round(valueOr0(object.snow)*100)/100,
+            temp: Math.round(object.main.temp - 273.15)*10/10,
+            wind: Math.round(object.wind.speed*100)/100,
+            bikeExplanation: "",
+            bikeDecision: false,
+            bike : function(dayParam){
+              if (userLimits.daysOff.find((number) => number === dayParam) !== undefined){
+                this.bikeExplanation = "week-end";
+              } else if (userLimits.rain < this.rain){
+                this.bikeExplanation = `rain ${this.rain}mm`;
+              } else if (userLimits.snow < this.snow){
+                this.bikeExplanation = `snow ${this.snow}mm`;
+              } else if (userLimits.wind < this.wind){
+                this.bikeExplanation = `too much wind ${this.wind}km/h`;
+              } else if (userLimits.temp < this.temp - 273.15){
+                this.bikeExplanation = `too cold ${this.temp}°C`
+              } else{
+                this.bikeDecision = true;
+              }
             }
-          }
-        };
-    });
-    returnedTimes.forEach((object) => {
-      object.bike(object.day);
-    })
+          };
+      });
+      returnedTimes.forEach((object) => {
+        object.bike(object.day);
+      })
 
-    let selectedTimes = returnedTimes.filter((object) => {
-      if (object.hour === "09" || object.hour === "21"){
-        return object;
+      let selectedTimes = returnedTimes.filter((object) => {
+        if (object.hour === "09" || object.hour === "21"){
+          return object;
+        }
+      });
+      if (selectedTimes[0].hour === "21"){// if it's already afternoon, you already took your decision
+        selectedTimes.splice(0,1);
       }
-    });
-    if (selectedTimes[0].hour === "21"){// if it's already afternoon, you already took your decision
-      selectedTimes.splice(0,1);
-    }
-    if (selectedTimes[selectedTimes.length - 1].hour === "09"){//21 not yet in the forecast
-      selectedTimes.pop();
-    }
-
-    let groupByDate = [];
-    for (let i = 0; i<selectedTimes.length; i = i+2){
-      jourTemp = selectedTimes[i].date;
-        groupByDate.push({
-          date: selectedTimes[i].date,
-          morningBike: selectedTimes[i].bikeDecision,
-          morningExplanation: selectedTimes[i].bikeExplanation,
-          eveningBike: selectedTimes[i+1].bikeDecision,
-          eveningExplanation: selectedTimes[i+1].bikeExplanation
-        });
-    }
-
-    let decision = groupByDate.map((object) => {
-      if (object.morningExplanation === "week-end"){
-        return `\n${object.date} week-end`;
-      } else if (object.morningBike && object.eveningBike) {
-        return `\n${object.date} 🚲`;
-      } else if (!object.morningBike && !object.eveningBike){
-        return `\n${object.date} 🚙 : ${object.morningExplanation} le matin et ${object.eveningExplanation} le soir.`;
-      } else if (!object.morningBike && object.eveningBike){
-        return `\n${object.date} 🚙 : ${object.morningExplanation} le matin.`;
-      } else if (object.morningBike && !object.eveningBike){
-        return `\n${object.date} 🚙 : ${object.eveningExplanation} le soir.`;
+      if (selectedTimes[selectedTimes.length - 1].hour === "09"){//21 not yet in the forecast
+        selectedTimes.pop();
       }
-    });
 
-    console.log("Décisions :" + decision.join(""));
+      let groupByDate = [];
+      for (let i = 0; i<selectedTimes.length; i = i+2){
+        jourTemp = selectedTimes[i].date;
+          groupByDate.push({
+            date: selectedTimes[i].date,
+            morningBike: selectedTimes[i].bikeDecision,
+            morningExplanation: selectedTimes[i].bikeExplanation,
+            eveningBike: selectedTimes[i+1].bikeDecision,
+            eveningExplanation: selectedTimes[i+1].bikeExplanation
+          });
+      }
+
+      let decision = groupByDate.map((object) => {
+        if (object.morningExplanation === "week-end"){
+          return `\n${object.date} week-end`;
+        } else if (object.morningBike && object.eveningBike) {
+          return `\n${object.date} 🚲`;
+        } else if (!object.morningBike && !object.eveningBike){
+          return `\n${object.date} 🚙 : ${object.morningExplanation} in the morning and ${object.eveningExplanation} in the evening.`;
+        } else if (!object.morningBike && object.eveningBike){
+          return `\n${object.date} 🚙 : ${object.morningExplanation} in the morning.`;
+        } else if (object.morningBike && !object.eveningBike){
+          return `\n${object.date} 🚙 : ${object.eveningExplanation} in the evening.`;
+        }
+      });
+
+      console.log("Decisions:" + decision.join(""));
+    }
+
 
   }
 );
