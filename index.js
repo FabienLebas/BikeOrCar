15ccdd162610bcc7865ba993faf4f6ba8df4a124
Fabien Lebas <fabien.lebas@decathlon.com>
2017-12-14 17:09:23 +0100

    First Express version

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const decision = require("./decision");
+
+const app = express();
+
+const port = process.env.PORT || 3000;
+app.listen(port, function () {
+  console.log("Server listening on port:" + port);
+});
+
+app.get("/", function (request, result) {
+  decision.getDataFromOpenWeather(result);
+});
