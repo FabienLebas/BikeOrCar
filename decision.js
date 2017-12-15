f34b9623c3f57e19c9f90ce6b6c421f22c561f85
Fabien Lebas <fabien.lebas@decathlon.com>
2017-12-15 17:46:28 +0100

    modif google analytics

--- decision.js
+++ decision.js
@@ -52,13 +52,14 @@ function displayDecisions(groupByDate){
   });
 
   localExpressResult.send("<head><title>Bike Or Car</title>"+
-    "<!-- Google Tag Manager -->" +
-  "<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','GTM-5HSTNPH');</script>" +
-  "<!-- End Google Tag Manager -->" +
-  "</head><body>" +
-  "<!-- Google Tag Manager (noscript) -->" +
-  "<noscript><iframe src='https://www.googletagmanager.com/ns.html?id=GTM-5HSTNPH' height='0' width='0' style='display:none;visibility:hidden'></iframe></noscript>" + 
-  "<!-- End Google Tag Manager (noscript) -->" +
+    "<!-- Global site tag (gtag.js) - Google Analytics -->" +
+    "<script async src='https://www.googletagmanager.com/gtag/js?id=UA-111318789-1'></script>" +
+    "<script>" +
+    "window.dataLayer = window.dataLayer || [];" +
+    "function gtag(){dataLayer.push(arguments);}" +
+    "gtag('js', new Date());" +
+    "gtag('config', 'UA-111318789-1');" +
+    "</script>" +
     decision.join("<br>") + "</body>");
 }
 
