e21b2ff3ac61bcafbed5d63228b1932cbf9afa19
Fabien Lebas <fabien.lebas@decathlon.com>
2017-12-15 09:24:46 +0100

    Added Google Analytics

--- decision.js
+++ decision.js
@@ -51,7 +51,15 @@ function displayDecisions(groupByDate){
     }
   });
 
-  localExpressResult.send(decision.join("<br>"));
+  localExpressResult.send("<head><title>Bike Or Car</title>"+
+    "<!-- Google Tag Manager -->" +
+  "<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','GTM-5HSTNPH');</script>" +
+  "<!-- End Google Tag Manager -->" +
+  "</head><body>" +
+  "<!-- Google Tag Manager (noscript) -->" +
+  "<noscript><iframe src='https://www.googletagmanager.com/ns.html?id=GTM-5HSTNPH' height='0' width='0' style='display:none;visibility:hidden'></iframe></noscript>" + 
+  "<!-- End Google Tag Manager (noscript) -->" +
+    decision.join("<br>") + "</body>");
 }
 
 function getDataFromOpenWeather(expressResult){
